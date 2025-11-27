826610e451a1a64b8645b2a6424ef33491e5054d
kauamassei <gameplayultra11@gmail.com>
2025-11-27 01:52:43 -0300

    fix(jwt)

--- src/controllers/public.js
+++ src/controllers/public.js
@@ -1,16 +1,15 @@
-import express from "express";
+
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 
 //Cadastro
 
-export async function createUserController (req, res){
+export async function createUserController(req, res) {
   try {
     const user = req.body;
 
@@ -35,35 +34,32 @@ export async function createUserController (req, res){
 //Login
 
 export async function loginUserController(req, res) {
-    try {
-        const userInfo = req.body;
-    
-        const user = await prisma.user.findUnique({
-          where: { email: userInfo.email },
-        });
-    
-        //verifica se usuario existe no banco
-        if (!user) {
-          res.status(404).json({ message: "Usuário não encontrado" });
-        }
-    
-        // compara a senha do banco com a que o usuário digitou
-        const isMatch = await bcrypt.compare(userInfo.password, user.password);
-        
-        if (!isMatch) {
-          res.status(400).json({ message: "Senha inválida" });
-        }
-    
-        // gera o token jwt
-    
-        const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: '7d'})
-    
-        res.status(200).json(token);
-    
-      } catch (error) {
-        console.log(error);
-        res.status(500).json({ message: "Erro no servidor, tente novamente" });
-      }
+  try {
+    const userInfo = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email },
+    });
+
+    //verifica se usuario existe no banco
+    if (!user) {
+      res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    // compara a senha do banco com a que o usuário digitou
+    const isMatch = await bcrypt.compare(userInfo.password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ message: "Senha inválida" });
+    }
+
+    // gera o token jwt
+
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "7d" });
+
+    res.status(200).json(token);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Erro no servidor, tente novamente" });
+  }
 }
-
-

c6a34c7683f1bd48d6b79fcaa76b9762ae0446cb
kauamassei <gameplayultra11@gmail.com>
2025-12-05 03:36:59 -0300

    feat: add showMeUserController and route

--- src/controllers/private.js
+++ src/controllers/private.js
@@ -1,7 +1,33 @@
-
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+export async function showMeUserController(req, res) {
+  try {
+    const userId = req.user.id;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+
+
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Falha ao buscar usuário" });
+  }
+  console.log("REQ.USER:", req.user);
+
+}
 
 export async function listUsersController(req, res) {
   try {
@@ -12,4 +38,3 @@ export async function listUsersController(req, res) {
     res.status(500).json({ message: "Falha no servidor" });
   }
 }
-
--- src/routes/privateRoutes.js
+++ src/routes/privateRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { listUsersController } from "../controllers/private.js";
+import { listUsersController, showMeUserController } from "../controllers/private.js";
 
 const router = Router();
 
 router.get("/listar-usuarios", listUsersController);
+
+router.get("/me", showMeUserController);
 
 export default router;

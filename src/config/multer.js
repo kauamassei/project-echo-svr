a76bb2b29ebae439c15eadb40ca7c94de94214dc
kauamassei <gameplayultra11@gmail.com>
2025-12-18 02:54:53 -0300

    feat: upload de avatar com multer e persistência no usuário

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,14 +2,16 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = path.resolve(process.cwd(), "uploads");
-      cb(null, uploadPath)
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
-  
- export const upload = multer({ storage: storage })
+  destination: function (req, file, cb) {
+    const uploadPath = path.resolve(process.cwd(), "uploads");
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname); // ← EXTENSÃO
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+
+    cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+  },
+});
+
+export const upload = multer({ storage });
--- src/controllers/private.js
+++ src/controllers/private.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import multer from "multer";
 
 const prisma = new PrismaClient();
-const upload = multer()
+const upload = multer();
 
 export async function showMeUserController(req, res) {
   try {
@@ -14,10 +14,10 @@ export async function showMeUserController(req, res) {
         id: true,
         name: true,
         email: true,
+        avatar: true,
+      
       },
     });
-
-
 
     if (!user) {
       return res.status(404).json({ message: "Usuário não encontrado" });
@@ -28,7 +28,6 @@ export async function showMeUserController(req, res) {
     res.status(500).json({ message: "Falha ao buscar usuário" });
   }
   console.log("REQ.USER:", req.user);
-
 }
 
 export async function listUsersController(req, res) {
@@ -43,15 +42,28 @@ export async function listUsersController(req, res) {
 
 export async function uploadImageController(req, res) {
   try {
+    const userId = req.user.id;
 
-    console.log(req.file)
+    if (!req.file) {
+      return res.status(400).json({ message: "Nenhuma imagem enviada" });
+    }
 
-    
+    const avatarPath = `http://localhost:3333/uploads/${req.file.filename}`
+    const user = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        avatar: avatarPath,
+      },
+    });
 
+    console.log(req.file);
 
-
-    res.status(200).json({message: 'Imagem atualizada!'})
+    return res.status(200).json({
+      message: "Imagem atualizada com sucesso!",
+      avatar: user.avatar,
+    });
   } catch (error) {
-    res.status(500).json({message: 'Falha no servidor'})
+    console.error(error);
+    return res.status(500).json({ message: "Falha no servidor" });
   }
 }
